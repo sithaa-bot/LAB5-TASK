aed9234b5cd3f977026dcb5f4a6f0ed6eb15f6f7
VengChhay <vengchay011@gmail.com>
2025-01-14 20:51:40 +0700

    task6

---
+++ start/Session.java
@@ -0,0 +1,27 @@
+/**
+ * Name class: Session
+ * Date: 14 January 2025
+ * Author: Taing Veng Chhay
+ * Collarborators: Huoth Sitha
+ * Modified: 14 January 2025
+ * Description: A class that testing the Subject class
+ * Functions:
+ * - setDuration(int, int): void
+ * Variables:
+ * -= startTime: int
+ * -= endTime: int
+ */
+public class Session {
+
+    private String sessionName;
+    private int startTime;
+    private int endTime;
+
+    public Session(String sessionName, int startTime, int endTime){
+        this.sessionName = sessionName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    
+}
